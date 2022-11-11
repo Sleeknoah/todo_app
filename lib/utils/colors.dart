0c1733deaa1f6f3f1934cf84ca4de458e531c3ee
Sleeknoah <yungdyke@gmail.com>
2022-11-12 00:04:54 +0100

    Setup commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const colorPrimary = Color(0xFF3084FE);
+const colorOrange = Color(0xFFFFC675);
+const colorPurple = Color(0xFFA4AAFE);
+const colorGreen = Color(0xFF8ED39D);
+const backgroundColorLight = Color(0xFFF9FAFC);
+const unselectedColor = Color(0xFFEBECF0);
---
+++ lib/utils/strings.dart
@@ -0,0 +1,4 @@
+const String personal = 'Personal';
+const String team = 'Teams';
+const dateHint = 'dd/mm/yyyy';
+const timeHint = 'hh : mm';
